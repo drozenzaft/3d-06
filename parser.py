60caddf5aef167fb8fcd95a2e756ac69c58f4ca7
Daniel Rozenzaft <drozenzaft@stuy.edu>
2018-03-16 22:45:36 -0400

    seems to work

--- parser.py
+++ parser.py
@@ -44,7 +44,7 @@ The file follows the following format:
 
 See the file script for an example of the file format
 """
-ARG_COMMANDS = [ 'line', 'scale', 'move', 'rotate', 'save', 'circle', 'bezier', 'hermite', 'box' ]
+ARG_COMMANDS = [ 'line', 'scale', 'move', 'rotate', 'save', 'circle', 'bezier', 'hermite', 'box', 'sphere', 'torus' ]
 
 def parse_file( fname, edges, transform, screen, color ):
 
@@ -111,7 +111,16 @@ def parse_file( fname, edges, transform, screen, color ):
         elif line == 'box':
             add_box(edges,float(args[0]),float(args[1]),float(args[2]),float
                     (args[3]),float(args[4]),float(args[5]))
-        
+            
+        elif line == 'sphere':
+            add_sphere(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), 0.05)
+
+        elif line == 'torus':
+            add_torus(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), float(args[4]), 0.04)
+
+        elif line == 'clear':
+            edges = []
+            
         elif line == 'apply':
             matrix_mult( transform, edges )
 
