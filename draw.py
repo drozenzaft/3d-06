e9e13c4665d85209294d604991e258a942540d7e
Daniel Rozenzaft <drozenzaft@stuy.edu>
2018-03-19 10:46:26 -0400

    fixed box coordinates

--- draw.py
+++ draw.py
@@ -4,17 +4,17 @@ from math import *
 
 def add_box( points, x, y, z, width, height, depth ):
     add_edge(points,x,y,z,x+width,y,z)
-    add_edge(points,x,y,z,x,y+height,z)
-    add_edge(points,x,y,z,x,y,z+depth)
-    add_edge(points,x+width,y,z,x+width,y+height,z)
-    add_edge(points,x+width,y,z,x+width,y,z+depth)
-    add_edge(points,x,y+height,z,x+width,y+height,z)
-    add_edge(points,x,y+height,z,x,y+height,z+depth)
-    add_edge(points,x,y,z+depth,x+width,y,z+depth)
-    add_edge(points,x,y,z+depth,x,y+height,z+depth)
-    add_edge(points,x,y+height,z+depth,x+width,y+height,z+depth)
-    add_edge(points,x+width,y,z+depth,x+width,y+height,z+depth)
-    add_edge(points,x+width,y+height,z,x+width,y+height,z+depth)
+    add_edge(points,x,y,z,x,y-height,z)
+    add_edge(points,x,y,z,x,y,z-depth)
+    add_edge(points,x+width,y,z,x+width,y-height,z)
+    add_edge(points,x+width,y,z,x+width,y,z-depth)
+    add_edge(points,x,y-height,z,x+width,y-height,z)
+    add_edge(points,x,y-height,z,x,y-height,z-depth)
+    add_edge(points,x,y,z-depth,x+width,y,z-depth)
+    add_edge(points,x,y,z-depth,x,y-height,z-depth)
+    add_edge(points,x,y-height,z-depth,x+width,y-height,z-depth)
+    add_edge(points,x+width,y,z-depth,x+width,y-height,z-depth)
+    add_edge(points,x+width,y-height,z,x+width,y-height,z-depth)
 
 def add_sphere( points, cx, cy, cz, r, step ):
     generate_sphere( points, cx, cy, cz, r, step)
